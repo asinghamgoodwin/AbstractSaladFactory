5e706ae83f18ddcf04b9b3133926f78b9fb71328
asinghamgoodwin <asinghamgoodwin@gmail.com>
2016-09-19 15:15:30 -0400

    updating the database

--- salads/models.py
+++ salads/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
-# Create your models here.
-#class Greeting(models.Model):
-#    when = models.DateTimeField('date created', auto_now_add=True)
+class Salad(models.Model):
+    salad_date = models.DateField()
+    salad_time = models.CharField(max_length=100)
+    salad_location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "Salad #"+str(self.id)+" @ "+self.salad_location
+
+class Ingredient(models.Model):
+    salad = models.ForeignKey(Salad, on_delete=models.CASCADE)
+    ingredient = models.CharField(max_length=100)
+    ingredient_type = models.CharField(max_length=100)
+    ingredient_owner = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.ingredient_owner+" is bringing "+self.ingredient+" which is a "+self.ingredient_type+" to... "+self.salad.__str__()
--- salads/views.py
+++ salads/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-#from .templates import test.html
-#from .models import Greeting
-
 def index(request):
 #    return HttpResponse('Hello from Alicia!')
     return render(request, 'test.html')
